1a9f62bc30d10dbcca49632dfc954e4eed980800
byronskout <vyskoutaris@hotmail.com>
2019-11-14 15:02:33 +0000

    Welcome view page + db links rendered

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -1,12 +1,13 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\Link;
 use Faker\Generator as Faker;
 
-$factory->define(Link::class, function (Faker $faker) {
+/* @var Illuminate\Database\Eloquent\Factory $factory */
+
+$factory->define(App\Link::class, function (Faker $faker) {
     return [
-        //
+        'title' => substr($faker->sentence(2), 0, -1),
+        'url' => $faker->url,
+        'description' => $faker->paragraph,
     ];
 });
